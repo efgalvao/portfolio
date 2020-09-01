15486f82b8bc28c36fa1c6900e136b8e501c2f02
efgalvao <solfieri@hotmail.com>
2020-09-01 18:05:41 -0300

    footer updated.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.project_index, name="home"),
-    path("contact", views.contact, name="contact"),
-    path("project", views.project_index, name="project_index"),
+    #path("", views.project_index, name="home"),
+    path("contact/", views.contact, name="contact"),
+    path("project/", views.project_index, name="project_index"),
     path("project/<int:pk>", views.project_detail, name="project_detail"),
 
 ]
