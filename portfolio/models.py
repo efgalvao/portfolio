6b686804fc44c24e76e7ee9e54866c595b0c45dc
efgalvao <solfieri@hotmail.com>
2020-09-02 18:22:34 -0300

    MySQL configured.

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,9 +1,16 @@
+import os
 from django.db import models
+from django.conf import settings
+
+
 
 class Project(models.Model):
     title = models.CharField("title", max_length=50)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/img")
+    technology = models.CharField(max_length=50)
+    image = models.FilePathField(path='/home/solfieri/Documentos/Projetos/portfolio/portfolio/static/img')
+
+    def __str__(self):
+        return self.title
 
 
