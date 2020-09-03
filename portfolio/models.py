73ee480b4585870baacf66c180da36508ddc7d7a
efgalvao <solfieri@hotmail.com>
2020-09-03 00:13:23 -0300

    Html pages updated.

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,6 +6,7 @@ from django.conf import settings
 
 class Project(models.Model):
     title = models.CharField("title", max_length=50)
+    brief = models.CharField("brief", max_length=50)
     description = models.TextField()
     technology = models.CharField(max_length=50)
     image = models.FilePathField(path='/home/solfieri/Documentos/Projetos/portfolio/portfolio/static/img')
--- portfolio/views.py
+++ portfolio/views.py
@@ -22,3 +22,6 @@ def home(request):
 
 def contact(request):
     return render(request, 'contact.html', {})
+
+def about(request):
+    return render(request, 'about.html', {})
