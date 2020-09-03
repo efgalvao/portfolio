40187fdb392b9d934f4a07a39ec769e0f8a0bb2b
efgalvao <solfieri@hotmail.com>
2020-09-03 00:12:39 -0300

    Brief description created at model.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     #path("", views.project_index, name="home"),
+    path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
     path("project/", views.project_index, name="project_index"),
     path("project/<int:pk>", views.project_detail, name="project_detail"),
